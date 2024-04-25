a80805189a597f813bfbaea49a3bd26bfb14e940
Aldo Puello Ramirez <72235838+Aldopuello1999@users.noreply.github.com>
2024-04-24 22:43:29 -0500

    implementacion de nuevas imagenes en los pokemones

--- resources/views/Pokemon/index.blade.php
+++ resources/views/Pokemon/index.blade.php
@@ -5,7 +5,7 @@
                 @foreach ($pokemons as $pokemon)
                 {{-- {{dd($pokemon)}} --}}
                 <div class="card" style="width: 18rem;">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{{ $loop->index + 1 }}.png" class="card-img-top">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/female/{{ $loop->index + 1 }}.png" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">{{ $pokemon->name }}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
